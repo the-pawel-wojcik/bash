b81d7a2640f3c12bf6a9e470795573a4bd242cce
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-03-27 13:19:30 -0400

    Update jpg2pdf

--- pdf/jpg2pdf.sh
+++ pdf/jpg2pdf.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-cnt=0
-for fname in *.jpg
-do 
-    new_name=$(printf %04d $cnt)
-    # new_name=${fname%%.jpg}  # if you would like to keep the jpg names
-    echo $new_name $fname
-    convert -density 1000x1000 -quality 40 -compress jpeg $fname ${new_name}.pdf
-    cnt=$(($cnt +1))
-done
+if [[ "$#" -ne 1 ]] 
+then
+    echo Usage: $(basename $0) 'filename.jpg|filename.png' >&2 
+    exit 1
+fi
 
-echo Bind the album into a single pdf with 
-echo 'pdftk *.pdf cat output together.pdf'
+if [[ ! -r "$1" ]]
+then
+    echo Non readable file "$1". >&2
+    exit 1
+fi
+
+convert -density 1000x1000 -quality 40 -compress jpeg $fname ${new_name}.pdf
