5a7bc8c2b6374b533cb6c9971d3731105a432fb6
Paweł Wójcik <pawel.wojcik5@gmail.com>
2025-03-27 11:54:30 -0400

    Improve the rename option

--- rename.sh
+++ rename.sh
@@ -1,3 +1,18 @@
 # Don't run it. Don't source it. Just retype.
-
 find . -type f -name '*old*' -exec rename 's/old/new/' {} +
+
+# Replace only the last part of the filename
+find . -name '*v1*' -exec rename 's/(.*)v1/\1v0/' '{}' ';'
+# rename works different on different distributions
+
+# A fun one from stackoverflow
+# https://stackoverflow.com/questions/79490486/rename-multiple-files-in-nested-directories/79498331#79498331
+# get files in sensible order, echo commands below to confirm
+find . -depth -name key_A | 
+while read -r f
+do 
+    post="${f##*old}"
+    pre="${f%old$post}"
+    echo mv "$f" "${pre}new$post"
+done
+
